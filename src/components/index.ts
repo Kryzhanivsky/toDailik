0e786a7ae9a9ce54026d0a36228ee5d57a80d95b
Roman Kryzhanivskyi <77616134+Kryzhanivsky@users.noreply.github.com>
2026-07-04 13:37:18 +0200

    Added basic routing
    * feat: #1 Configured Tanstack Router library, created necessary pages and basic navigation
    
    * feat: created a custom side menu link

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export { MainLayout } from './layouts/main-layout/index.tsx'
+
+export { SideLink } from './shared/side-link/index.tsx'
---
+++ src/utils/Icon.tsx
@@ -0,0 +1,23 @@
+import * as Icons from '@heroicons/react/24/outline'
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+const iconRegistry = {
+  ChevronDoubleRightIcon: Icons.ChevronDoubleRightIcon,
+  ListBulletIcon: Icons.ListBulletIcon,
+  DocumentIcon: Icons.DocumentIcon,
+}
+
+export type IconName = keyof typeof iconRegistry
+
+interface IconProps extends React.ComponentPropsWithoutRef<'svg'> {
+  name: IconName
+}
+
+export const Icon = ({ name, className, ...other }: IconProps) => {
+  const IconComponent = iconRegistry[name]
+
+  if (!IconComponent) return null
+
+  return <IconComponent className={twMerge('size-5', className)} {...other} />
+}
